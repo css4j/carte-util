cecf127108debdd54d161f81d87ff19fcbd67db5
Carlos Amengual <carlosame644+github@gmail.com>
2020-12-17 20:58:33 +0100

    Add the Visitor<T> interface.

---
+++ src/io/sf/carte/util/Visitor.java
@@ -0,0 +1,28 @@
+/*
+
+ Copyright (c) 2005-2020, Carlos Amengual.
+
+ SPDX-License-Identifier: BSD-3-Clause
+
+ Licensed under a BSD-style License. You can find the license here:
+ https://css4j.github.io/LICENSE.txt
+
+ */
+
+package io.sf.carte.util;
+
+/**
+ * A visitor (from the 'visitor' pattern).
+ * 
+ * @param <T> the type to visit.
+ */
+public interface Visitor<T> {
+
+	/**
+	 * Visit the given object.
+	 * 
+	 * @param arg the object.
+	 */
+	void visit(T arg);
+
+}
