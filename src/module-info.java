d6de39758bbf0d7b6203f476c35c9ac43882a2aa
Carlos Amengual <carlosame644+github@gmail.com>
2025-12-13 20:39:16 +0100

    Put SPDX header on its own header, add BSD-2-Clause choice

--- src/module-info.java
+++ src/module-info.java
@@ -2,12 +2,13 @@
  *
  * Copyright (c) 2005-2025, Carlos Amengual.
  *
- * SPDX-License-Identifier: BSD-3-Clause
- *
  * Licensed under a BSD-style License. You can find the license here:
  * https://css4j.github.io/LICENSE.txt
  *
  */
+ /*
+   SPDX-License-Identifier: BSD-3-Clause OR BSD-2-Clause
+  */
 
 /**
  * Utility packages.
